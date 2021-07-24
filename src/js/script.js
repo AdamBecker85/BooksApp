2943e35ebb23079949e3825674550d207b228b25
Adam Becker <amkbecker@gmail.com>
2021-07-24 19:55:53 +0200

    Add initActions function

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,47 @@
 {
-    'use strict';
+  'use strict';
 
-    const select = {
-       templateOfBook: '#template-book',
-       listOfBooks: '.books-list',
+  const select = {
+    templateOfBook: '#template-book',
+    listOfBooks: '.books-list',
+    bookImage: '.book__image',
+  };
+
+  const templates = {
+    boook: Handlebars.compile(document.querySelector(select.templateOfBook).innerHTML),
+  };
+
+  function render(){
+    
+    for(let book of dataSource.books){
+      const generatedHTML = templates.boook(book);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      const bookToShow = document.querySelector(select.listOfBooks);
+      bookToShow.appendChild(generatedDOM);
     }
-
-    const templates = {
-        boook: Handlebars.compile(document.querySelector(select.templateOfBook).innerHTML),
-    }
-
-    function render(){
         
-        for(let book of dataSource.books){
-            const generatedHTML = templates.boook(book);
-            const generatedDOM = utils.createDOMFromHTML(generatedHTML);
-            const bookToShow = document.querySelector(select.listOfBooks);
-            bookToShow.appendChild(generatedDOM);
-        }
-        
-    }
+  }
 
-    render();
+  render();
+
+  const favoriteBooks = [];
+
+  function initActions(){
+    debugger;
+    const clickableBookImages = document.querySelector(select.bookImage);
+    
+    for(let imagee in clickableBookImages){
+
+      imagee.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        imagee.classList.add('favorite');
+        const dataID = imagee.getAttribute('data-id');
+        favoriteBooks.push(dataID);
+      });
+    }
+       
+  }
+  
+  
 
 }
